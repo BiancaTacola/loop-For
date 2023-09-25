7a7a18c05ade03df38628741e28f1ad15cc604f4
Bianca Tacola <95148673+BiancaTacola@users.noreply.github.com>
2023-09-25 20:41:16 -0300

    Add files via upload
    Este código simplesmente imprime os números de 0 a 10 na tela, demonstrando o uso de um loop for em Java.

---
+++ For.java
@@ -0,0 +1,21 @@
+// Importa a classe "ExemploFor" para o pacote "com.fiap.view"
+package com.fiap.view;
+
+// Declaração da classe "ExemploFor"
+public class For {
+    
+    // Método principal (ponto de entrada) do programa
+    public static void main(String[] args) {
+        
+        // Inicia um loop "for" que irá contar de 0 a 10 (inclusive)
+        for (int i = 0; i <= 10; i++) {
+            
+            // Imprime o valor atual de "i" na tela
+            System.out.println(i);
+        }
+        
+        // Fim do método principal
+    }
+    
+    // Fim da classe "ExemploFor"
+}
